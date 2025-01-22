e3554a58ab7f0ed6642e7c10ec20ad6c45f0cadf
alexjung417 <alexanderjung417@gmail.com>
2025-01-22 11:44:59 -0800

    Example AWS Login

---
+++ AWS.R
@@ -0,0 +1,39 @@
+library(dplyr)
+library(readr)
+library(purrr)
+library(stringr)
+library(tibble)
+library(magick)
+library(aws.s3)
+
+# This is a list of the commands you call call 
+ls("package:aws.s3")
+
+
+# use this it test if you get back the correct code
+bucket_exists(
+  bucket = "s3://herbariumnsw-pds/", 
+  region = "ap-southeast-2"
+)
+# [1] TRUE
+# attr(,"x-amz-id-2")
+
+#Change this based on how deep you want to go
+Bucket <- "s3://whale-recordings/CPhydrophone/Avila/" #check if you can access the bucket
+
+Bucket_Deployment_2 <- "s3://whale-recordings/CPhydrophone/Avila/Deployment 2/wav-files/fullsize_files/"
+
+
+bucket_exists(
+  bucket = Bucket,
+  key = YOUR_AWS_ACCESS_KEY,
+  secret = YOUR_AWS_SECRET_ACCESS_KEY,
+  region = AWS_DEFAULT_REGION
+)
+
+get_bucket_df(
+  bucket = Bucket_Deployment_2,
+  key = YOUR_AWS_ACCESS_KEY,
+  secret = YOUR_AWS_SECRET_ACCESS_KEY,
+  region = AWS_DEFAULT_REGION
+)
