9173e1fd03cf8f5b6452b5b21c1cfb84f496fc1c
alexjung417 <alexanderjung417@gmail.com>
2025-02-25 15:47:30 -0800

    Automated creating a master csv for FFT

--- Data Capstone Reading Wav Files.R
+++ Data Capstone Reading Wav Files.R
@@ -1,3 +1,4 @@
+# ------------------------- Fast Fourier Transform -----------------------------
 # used to fast fourier transform the wav files and make them tabular
 wav_to_frequency_csv <- function(wav_path,
                                  time_period, # This is in seconds 
@@ -79,7 +80,7 @@ wav_to_frequency_csv <- function(wav_path,
   return(df)
 }
 
-# create the freq ranges
+# ------------------------ create the freq ranges ------------------------------
 generate_freq_ranges <- function(end_freq, # where do you want the freq to stop
                                  step_size) {
   num_ranges <- ceiling(end_freq / step_size)
@@ -92,19 +93,19 @@ generate_freq_ranges <- function(end_freq, # where do you want the freq to stop
   return(freq_ranges)
 }
 
-# can read in data from AWS and then merge annotations with tabular data
+# -------------------- can read in data from AWS -------------------------------
 merging_data <- function(name, end_freq, step_size, 
                          time_period = .1, # you can change this
                          start_time = NULL, end_time = NULL, # to shrink around one part if necessary
                          annotated = FALSE # This is for when the data is not annotated we can still read in the files into csv form
-                         ){
+){
   freq_ranges <- generate_freq_ranges(end_freq = end_freq, step_size = step_size)
   
   # grabbing the wav file
   grab_wav_files(name)
   
   # creating the data frame
-  wav_file <- wav_to_frequency_csv(name, time_period, freq_ranges,start_time, end_time)
+  wav_file <- wav_to_frequency_csv(name, time_period, freq_ranges, start_time, end_time)
   
   # if there are annotations reading in the annotations txt
   if (annotated){
@@ -124,12 +125,13 @@ merging_data <- function(name, end_freq, step_size,
                               wav_file$song)
       # annotation (selection) number
       wav_file$annotation_num <- ifelse(wav_file$time_start >= begin & wav_file$time_start <= end | wav_file$time_end >= begin & wav_file$time_end <= end,
-                                   i,
-                                   wav_file$annotation_num)
+                                        i,
+                                        wav_file$annotation_num)
     }
     wav_file$song <- factor(wav_file$song, levels = c(1,0))
   }
   
+  wav_file$time_interval <- rleid(wav_file$annotation_num)
   # deleting the file to save space
   if (file.exists(name)) {
     file.remove(name)
@@ -137,6 +139,7 @@ merging_data <- function(name, end_freq, step_size,
   return (wav_file)
 }
 
+# ------------------------------ Documentation ---------------------------------
 # for not annotated data can also use wav_to_frequency_csv but this would require you to have downloaded the data prior
 
 
@@ -144,3 +147,83 @@ merging_data <- function(name, end_freq, step_size,
 # file <- "6805.230201090825.wav"   # need to change this to the files that you want to pull
 
 # df <- merging_data(file, 800, 50)
+
+# ---------------------------- FFT to CSV From AWS -----------------------------
+fft_to_csv <- function(name, end_freq, step_size, 
+                         time_period = .1, # you can change this
+                         start_time = NULL, end_time = NULL, # to shrink around one part if necessary
+                         annotated = FALSE # This is for when the data is not annotated we can still read in the files into csv form
+                       ){
+  freq_ranges <- generate_freq_ranges(end_freq = end_freq, step_size = step_size)
+  
+  # grabbing the wav file
+  grab_wav_files(name)
+  
+  # creating the data frame
+  wav_file <- wav_to_frequency_csv(name, time_period, freq_ranges, start_time, end_time)
+  
+  # if there are annotations reading in the annotations txt
+  if (annotated){
+    txt_name <- sub("\\.wav$", "-SS", name)
+    target <- grab_txt_files(txt_name)
+    
+    # adding the indicator column & annotation (selection) number
+    wav_file$song <- 0
+    wav_file$annotation_num <- 0
+    for(i in 1:nrow(target)){
+      begin <- target$begin_time[i]
+      end <- target$end_time[i]
+      
+      # target column
+      wav_file$song <- ifelse(wav_file$time_start >= begin & wav_file$time_start <= end | wav_file$time_end >= begin & wav_file$time_end <= end,
+                              1,
+                              wav_file$song)
+      # annotation (selection) number
+      wav_file$annotation_num <- ifelse(wav_file$time_start >= begin & wav_file$time_start <= end | wav_file$time_end >= begin & wav_file$time_end <= end,
+                                        i,
+                                        wav_file$annotation_num)
+    }
+    wav_file$song <- factor(wav_file$song, levels = c(1,0))
+  }
+  
+  wav_file$time_interval <- rleid(wav_file$annotation_num)
+  # deleting the file to save space
+  if (file.exists(name)) {
+    file.remove(name)
+  }
+  file_name <- sub("\\.wav$", "_fft.csv", name)
+  write.csv(wav_file, file_name)
+  return ('done')
+}
+
+# ----------------------- Creation of fft_csv ----------------------------------
+wavs_names <- read.table("Wav file names", header = TRUE, sep = " ", colClasses = c("character", "numeric"))
+wavs <- as.list(wavs_names$Filename)
+
+for (i in wavs_names) {
+  name <- paste0(i, ".wav")
+  merging_data_melfcc_csv(name,numcep = 13, wintime = .2, hoptime = .1, maxfreq = 800, annotated = TRUE)
+  rm(wav_file)
+  gc()
+  
+}
+
+# combining to one master csv 
+# can edit the training one
+
+test <- c(31,27,29)
+validation <- c(17, 22,32,28,23)
+all_indices <- 1:38
+remaining <- setdiff(all_indices, c(test, validation))
+
+train_fft <- data.frame()
+for(i in remaining){
+  wav_name <- wavs[[i]]
+  name <- paste0(wav_name, "_fft.csv")
+  file <- read.csv(name)
+  train_fft <- rbind(train_fft,file )
+}
+write.csv(train_fft, "train_fft.csv")
+
+# need to change the number of songs in validation and test
+# assign new annotation numbers so there won't be 2 annotation number 2
