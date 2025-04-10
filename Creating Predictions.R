6874d9e549b3d702db7e0a504812cb7c0debdb37
alexjung417 <alexanderjung417@gmail.com>
2025-04-10 16:11:14 -0700

    Made create selection table easier to use.

--- Creating Predictions.R
+++ Creating Predictions.R
@@ -4,16 +4,14 @@
   source("AWS.R")
   source("Data Capstone Reading Wav Files.R")
   set.seed(1738)
-  
   # library(keras)
   # library(tensorflow)
-  library(seewave)
   library(here)
 }
 
 
 
-# do not run need to update the helper function accomodate the need rds fitted models
+# do not run need to update the helper function accommodate the need rds fitted models
 
 # -------------------- helper functions for the one below ----------------------
 merging_data_stft <- function(audio, n = 16384, overlap = 6784, freq = 51, window = NULL){
@@ -31,25 +29,36 @@ merging_data_stft <- function(audio, n = 16384, overlap = 6784, freq = 51, windo
   return (wav_file)
 }
 
-stft_pipeline <- function(audio){
+stft_pipeline <- function(audio, model){
   df <- merging_data_stft(audio)
-  rf_stft <- readRDS("rf_final_melfcc.rds")
+  
+  rf_stft <- readRDS("rf_final_stft.rds")
   df$rf.pred.stft <- predict(rf_stft, new_data = df)$.pred_class
   rm(rf_stft)
-  # This is for K Nearest Neighbors
   knn_stft <- readRDS("knn_final_stft.rds")
   df$knn.pred.stft <- predict(knn_stft, new_data = df)$.pred_class
   rm(knn_stft)
-  df <- df |> 
-    dplyr::select("time_start","time_end","knn.pred.stft","rf.pred.stft")
+  
+  # This is for random forest
+  if(model == "rf"){
+    df <- df |> 
+      dplyr::select("time_start","time_end","rf.pred.stft")
+  }
+  # This is for K Nearest Neighbors
+  if(model == "knn"){
+    df <- df |> 
+      dplyr::select("time_start","time_end","knn.pred.stft")
+  }
+  if (model == 'both'){
+    df <- df |> 
+      dplyr::select("time_start","time_end","knn.pred.stft","rf.pred.stft")
+  }
   print("STFT is complete")
   return(df)
 }
 
-
-
-merging_data_melfcc <- function(audio, numcep, maxfreq, wintime, hoptime){
-  audio2 <- melfcc(audio, numcep = numcep, maxfreq = maxfreq, wintime = wintime, hoptime = hoptime)
+merging_data_melfcc <- function(audio){
+  audio2 <- melfcc(audio, numcep = 13, maxfreq = 600, wintime = .2, hoptime = .1)
   
   wav_file <- as.data.frame(audio2) %>%
     mutate(
@@ -59,88 +68,97 @@ merging_data_melfcc <- function(audio, numcep, maxfreq, wintime, hoptime){
   return (wav_file)
 }
 
-melfcc_pipeline <- function(audio, numcep, maxfreq, wintime, hoptime){
-  df <- merging_data_melfcc(audio, numcep, maxfreq, wintime, hoptime)
+melfcc_pipeline <- function(audio, model ){
+  df <- merging_data_melfcc(audio)
   rf_melfcc <- readRDS("rf_final_melfcc.rds")
   df$rf.pred.mel <- predict(rf_melfcc, new_data = df)$.pred_class
   rm(rf_melfcc)
-  # This is for K Nearest Neighbors
   knn_melfcc <- readRDS("knn_final_melfcc.rds")
   df$knn.pred.mel <- predict(knn_melfcc, new_data = df)$.pred_class
   rm(knn_melfcc)
-  gc()
-  df <- df |> 
-    dplyr::select("time_start","time_end","knn.pred.mel","rf.pred.mel")
+  
+  # This is for random forest 
+  if ( model == "rf") {
+    df <- df |> 
+      dplyr::select("time_start","time_end","rf.pred.mel")
+  }
+  # This is for K Nearest Neighbors
+  if (model == "knn"){
+    df <- df |> 
+      dplyr::select("time_start","time_end","knn.pred.mel")
+  }
+  if (model == 'both'){
+    df <- df |> 
+      dplyr::select("time_start","time_end","knn.pred.mel","rf.pred.mel")
+  }
   print("MELFCC is complete")
   return(df)
 }
 
-
-
 # need to change this to make sure the second part works
-to_selection_table <- function(df, name){
-  predictions.v1 <- df |>
-    mutate(total.pred = as.numeric(levels(knn.pred.mel))[knn.pred.mel] +
-             as.numeric(levels(knn.pred.stft))[knn.pred.stft] +
-             as.numeric(levels(rf.pred.stft))[rf.pred.stft] +
-             as.numeric(levels(rf.pred.mel))[rf.pred.mel] ,
-           rolling.avg = frollmean(total.pred, n = 30, fill = 0),
-           final_pred = ifelse(rolling.avg>0, 1, 0),
-           pred_number= rleid(final_pred),
-           `Begin Time (s)` = time_start, - 2,
-           `End Time (s)` = time_end - 2) #adjust timeframe proportional to n (3 is too far)
-  predictions.v1 <- predictions.v1 |> 
-    filter(final_pred == 1) |> 
-    group_by(pred_number) |> 
-    summarise( `Begin Time (s)`= min(`Begin Time (s)`),
-               `End Time (s)` = max(`End Time (s)`)) |>
-    mutate(Selection = row_number(),
-           View = "Spectrogram 1",
-           Channel = 1) |> 
-    dplyr::select(Selection,View, Channel, `Begin Time (s)`, `End Time (s)`)
-  predictions.30 <- df |>
-    mutate(total.pred = as.numeric(levels(knn.pred.mel))[knn.pred.mel] +
-             as.numeric(levels(knn.pred.stft))[knn.pred.stft] +
-             as.numeric(levels(rf.pred.stft))[rf.pred.stft] +
-             as.numeric(levels(rf.pred.mel))[rf.pred.mel] ,
-           rolling.avg = frollmean(total.pred, n = 30, fill = 0),
-           final_pred = ifelse(rolling.avg>0, 1, 0),
-           pred_number= rleid(final_pred),
-           `Begin Time (s)` = time_start, - 2,
-           `End Time (s)` = time_end - 2) #adjust timeframe proportional to n (3 is too far)
-  predictions.30 <- predictions.30 |> 
-    filter(final_pred == 1) |> 
-    group_by(pred_number) |> 
-    summarise( `Begin Time (s)`= min(`Begin Time (s)`),
-               `End Time (s)` = max(`End Time (s)`)) |>
-    mutate(Selection = row_number(),
-           View = "Spectrogram 1",
-           Channel = 1) |> 
-    dplyr::select(Selection,View, Channel, `Begin Time (s)`, `End Time (s)`)
-  # write model predictions to .txt
-  # this can be put straight into raven
-  write_tsv(selection.table, paste0("pred.15sec.",name ,".txt")) 
+to_selection_table <- function(df, name, t){
+  df$total.pred <- 0
+  possible_preds <- c("knn.pred.mel", "knn.pred.stft", "rf.pred.stft", "rf.pred.mel")
+  for(pred in possible_preds) {
+    if(pred %in% colnames(df)) {
+      df$total.pred <- df$total.pred + as.numeric(levels(df[[pred]]))[df[[pred]]]
+    }
+  }
+  for (i in t){
+    predictions.v1 <- df |>
+      # need to figure out how to only make it so the correct pred variables are used otherwise it will give error
+      mutate(rolling.avg = frollmean(total.pred, n = t, fill = 0),
+             final_pred = ifelse(rolling.avg>0, 1, 0),
+             pred_number= rleid(final_pred),
+             `Begin Time (s)` = time_start, - (t/10),
+             `End Time (s)` = time_end - (t/10)) #adjust timeframe proportional to n (3 is too far)
+    selection.table <- predictions.v1 |> 
+      filter(final_pred == 1) |> 
+      group_by(pred_number) |> 
+      summarise( `Begin Time (s)`= min(`Begin Time (s)`),
+                 `End Time (s)` = max(`End Time (s)`)) |>
+      mutate(Selection = row_number(),
+             View = "Spectrogram 1",
+             Channel = 1) |> 
+      dplyr::select(Selection,View, Channel, `Begin Time (s)`, `End Time (s)`)
+    # write model predictions to .txt
+    # this can be put straight into raven
+    write_tsv(selection.table, paste0("preds.",(t/10),"sec.",name ,".txt")) 
+  }
+
   # write_tsv(predictions.30, paste0("pred.30sec.",name ,".txt"))
 }
 
 # ----------------------- Function output selection tables ---------------------
 
 create_selection_table <- function(name, # ex. "6805.230201090825.wav"
-                                   mtry_n, min_n_n, n, # ex. df_stft_train_10_files.csv, 10,20,30
-                                   numcep, maxfreq, wintime, hoptime, # train_melfcc.csv
-                                   mtry_n_m, min_n_n_m, n_m
-){
+                                   # need to add a input of which model and which transformation
+                                   model, transformation, 
+                                   t){
   grab_wav_files(name)
   file_audio <- readWave(name)
-  # file_audio_stft <- stft_pipeline(file_audio,mtry_n, min_n_n, n)
-  file_audio_melfcc <- melfcc_pipeline(file_audio, numcep, maxfreq, wintime, hoptime, mtry_n_m, min_n_n_m, n_m)
+  
+  # This is for stft 
+  if( transformation == "stft"){
+    file_predictions <- stft_pipeline(file_audio, model)
+  }
+  # This is for melfcc
+  if (transformation == "melfcc"){
+    file_predictions <- melfcc_pipeline(file_audio, model)
+  }
+  if (transformation == "both"){
+    file_audio_stft <- stft_pipeline(file_audio, model)
+    file_audio_melfcc <- melfcc_pipeline(file_audio, numcep, maxfreq, wintime, hoptime, model)
+    file_predictions <- merge(file_audio_stft, file_audio_melfcc, by = (c("time_start","time_end")), all.x = TRUE)
+  }
   rm(file_audio)
-  # file_predictions <- merge(file_audio_stft, file_audio_melfcc, by = (c("time_start","time_end")), all.x = TRUE)
-  # file_predictions[is.na(file_predictions)] <- 0
-  # final_name <- sub("\\.wav$", name)
-  # write.csv(file_predictions, paste0("pred.",name, ".csv"))
-  # to_selection_table(file_predictions, name = final_name)
-  # return(file_predictions)
+  
+  # i need to make sure that the predictions only happen for the specific predictions names
+  file_predictions[is.na(file_predictions)] <- 0
+  #write.csv(file_predictions, paste0("pred.",name, ".csv"))
+  name_1 <- sub("\\.wav$", name)
+  to_selection_table(file_predictions, name = name_1, t)
+  return(file_predictions)
 }
 name <- "6805.230205170826.wav"
 
@@ -148,10 +166,50 @@ name <- "6805.230205170826.wav"
 # min_n_n <-  30
 # 
 # n <- 30
-# create_selection_table(name = name, mtry_n = 15, min_n_n = 30, n = 30,
-#                       numcep = 13, maxfreq = 600, wintime = .2, hoptime = .1,
-#                        mtry_n_m = 10, min_n_n_m = 33, n_m = 30)
 
+# model can be stft, melfcc, or both
+# transformation can be rf, knn, or both
+file_predictions <- create_selection_table(name = name, model = "", transformation = "",  
+                                           t = c(50, 400))
+
+# readWave(name, from = 0, to = 30, units = "minutes")
+# use this for humpback whale files in order to cut down on the file size
+
+
+# ---------------------- This is not needed to be run --------------------------
+
+# melfcc_valid_predictions <- read.csv("melfcc_val_res_full.csv")
+# melfcc_valid_predictions_checking <- melfcc_valid_predictions |> 
+#   filter(filenumber == 2)
+# 6805.230206100827
+# get_model_predictions_mel <- function(preds,t = 100, write = T){
+#   # extract filename for output
+#   output <- preds |> 
+#     # add other models into total.pred
+#     mutate(total.pred = as.numeric(knn.pred) + 
+#              as.numeric(rf.pred),
+#            rolling.avg = frollmean(total.pred, n = t, fill = 0),
+#            final_pred = ifelse(rolling.avg>0, 1, 0),
+#            pred_number= rleid(final_pred),
+#            `Begin Time (s)` = time_start, - (t/10),
+#            `End Time (s)` = time_end - (t/10) ) |> 
+#     filter(final_pred == 1) |> 
+#     group_by(pred_number) |> 
+#     summarise( `Begin Time (s)`= min(`Begin Time (s)`),
+#                `End Time (s)` = max(`End Time (s)`)) |>
+#     mutate(Selection = row_number(),
+#            View = "Spectrogram 1",
+#            Channel = 1) |> 
+#     dplyr::select(Selection,View, Channel, `Begin Time (s)`, `End Time (s)`)
+#   
+#   path <- paste0("model.pred.6805.230206100827.txt", sep = "")
+#   if(write){
+#     write_tsv(output, path)
+#   }
+#   output
+# }
+# 
+# get_model_predictions_mel(melfcc_valid_predictions)
 
 # ------------------------ Setting up the Fitted models ------------------------
 
@@ -241,3 +299,6 @@ name <- "6805.230205170826.wav"
 
 # example
 # knn_final_stft <- readRDS("knn_final_stft.rds")
+
+
+
